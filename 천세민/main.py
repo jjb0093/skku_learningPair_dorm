7ab222aa59e2efb1412a73e734b6e434a14009be
cheonsemin <seminkev7@gmail.com>
2022-11-12 04:07:00 +0900

    천세민 weather.py 추가 @221112 am 4:06

--- 천세민/main.py
+++ 천세민/main.py
@@ -11,11 +11,54 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        postpage = QGridLayout()
+        pixmap1 = QPixmap('box.png')
+
+        boximage = QLabel()
+        boximage.setPixmap(pixmap1)
+
+        postpage = QVBoxLayout()
+        postpage.addStretch(1)
         postpage.addWidget(QLabel("택배조회"))
+        postpage.addStretch(3)
+        postpage.addWidget(boximage)
+        postpage.addStretch(4)
 
-        postAndFood = QHBoxLayout()
+        foodpage = QVBoxLayout()
+        foodpage.addStretch(1)
+        foodpage.addWidget(QLabel("식단"))
+        foodpage.addStretch(3)
+        foodpage.addWidget(boximage)
+        foodpage.addStretch(4)
 
+        postandfood = QGridLayout()
+        postandfood.addChildLayout(postpage, 0, 0)
+        postandfood.addChildLayout(foodpage, 0, 1)
+
+        weatherpage = QVBoxLayout()
+        weatherpage.addStretch(1)
+        weatherpage.addWidget(QLabel("날씨"))
+        weatherpage.addStretch(3)
+        weatherpage.addWidget(boximage)
+        weatherpage.addStretch(3)
+
+        rightgrid = QGridLayout()
+        rightgrid.addChildLayout(weatherpage, 0, 0)
+        rightgrid.addChildLayout(postandfood, 1, 0)
+
+        noticeSeoul = QGridLayout()
+        noticeSeoul.addWidget(QLabel("인문사회과학캠퍼스"))
+        noticeSeoul.addWidget(QLabel("기숙사 공지사항"))
+
+        noticeSuwon = QVBoxLayout()
+        noticeSuwon.addStretch(1)
+        noticeSuwon.addWidget()
+
+        notice = QVBoxLayout()
+
+
+        allgrid = QGridLayout()
+        allgrid.addChildLayout(0, 0)
+        allgrid.addChildLayout(rightgrid, 0, 1)
 
         self.setWindowTitle('Main')
         self.setGeometry(300, 100, 800, 600)
